b4f2cfac135cfeccd987140b0a1ac0a8cd749e1b
Eduardo Taniguchi <edtaniguchi@gmail.com>
2020-06-30 21:49:43 -0300

    Criação da terceira mensagem e alteração das anteriores

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,8 +5,8 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Commit no git");
-		System.out.println("Segundo commit");
+		System.out.println("Bom dia");
+		System.out.println("Boa tarde");
+		System.out.println("Boa noite");
 	}
-
 }
